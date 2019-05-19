32ce0199e3bff63a6520aa6f2aadfd83aa4422f8
Paul Hebert <paul@cloudfour.com>
2019-05-19 10:22:53 -0700

    Implemented basic contrast checker

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,53 @@
 var Color = require("color");
+
+const colorInputs = document.querySelectorAll('.color-input');
+
+const foregroundInput = document.getElementById('foreground');
+const backgroundInput = document.getElementById('background');
+const example = document.getElementById('example');
+const contrast = document.getElementById('contrast');
+
+init();
+
+function init(){
+    updateColors();
+
+    colorInputs.forEach(input => {
+        input.addEventListener('input', () => {
+            updateColors();
+        });
+    })
+}
+
+function updateColors(){
+    if(isValid(foregroundInput.value) && isValid(foregroundInput.value)) {
+        example.style.background = background();
+        example.style.color = foreground();
+
+        // ToDo: Investigate odd edge cases mentioned here:
+        // https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
+        contrast.innerText = Color(background()).contrast(Color(foreground())).toFixed(2);
+    }
+}
+
+function isValid(color) {
+    let valid = true;
+
+    // TODO: Figure out why rollup doesn't like this and fix it
+    // Or, even better, figure out how to validate a color in Color.js
+    // try {
+    //     Color(color);
+    // } catch {
+    //     valid = false;
+    // }
+
+    return valid;
+};
+
+function background() {
+    return backgroundInput.value;
+}
+
+function foreground() {
+    return foregroundInput.value;
+}
