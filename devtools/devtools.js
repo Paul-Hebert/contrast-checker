36d9a66470942bd3870b6a27650c9c20f23f1382
Paul-Hebert <paul.hebert@paulhebertdesigns.com>
2019-05-25 09:55:04 -0700

    Exploring moving to dev  tools

---
+++ devtools/devtools.js
@@ -0,0 +1,9 @@
+window.browser = (function () {
+    return window.msBrowser || window.browser || window.chrome;
+})();
+
+window.browser.devtools.panels.create(
+    "Contrast Checker",
+    "../icons/icon.png",
+    "../index.html"
+);
